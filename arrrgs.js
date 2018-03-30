65bda8dbcd3b36ac53db58a0728168b2d096203d
Evan Genest <genest.1@osu.edu>
2018-03-30 06:53:18 -0500

    Oops... (a,b)=>  ECMA6 didn't work.  Use old syntax for functions to make the syntax for passing in args work.

--- arrrgs.js
+++ arrrgs.js
@@ -4,8 +4,25 @@
 /*  by mistergenest@twitter.com */
 /* March 30, 2018        */
 
-let cicero = ((x, y, z)=>{
-	console.log("Cicero speaks!");
-}  );
-cicero();
 
+let cicero = function(x, y, z){
+	console.log("Cicero speaks!");
+	console.log(`Citizens, anticipate _${arguments.length}_ things.`);
+	console.log(arguments[0]);
+}  ;
+cicero();
+console.log("and then,");
+cicero('Herrick Lane', '1', 1);
+/*==============================
+function func1(a, b, c) {
+  console.log(arguments[0]);
+  // expected output: 1
+
+  console.log(arguments[1]);
+  // expected output: 2
+
+  console.log(arguments[2]);
+  // expected output: 3
+}
+func1('anchovy','bread','celery');
+============================*/
