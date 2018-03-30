5771f00326436320294d5f67f48a558ce3037161
Evan Genest <genest.1@osu.edu>
2018-03-30 06:24:17 -0500

    Quick tester for accessing arg[0], arg[1] per reading the MDN.org page.

---
+++ arrrgs.js
@@ -0,0 +1,11 @@
+/* Just a quick test to see */
+/* whether I understand the args */
+/* passed into a function.*/
+/*  by mistergenest@twitter.com */
+/* March 30, 2018        */
+
+let cicero = ((x, y, z)=>{
+	console.log("Cicero speaks!");
+}  );
+cicero();
+
