7506cabd3ce7119c6dff0bf4c9a14081ad31b57d
Evan Genest <genest.1@osu.edu>
2018-03-31 10:13:06 -0500

    Make a snippet that catches UNDEFINED errors in the function args.

---
+++ arrrgs2.js
@@ -0,0 +1,9 @@
+let length1;
+if (undefined === length1){
+	length1 = [];
+}
+console.log(`Hey the array length result is now _${length1.length}_`);
+
+// this seems to work
+
+// error catch for undefined args
