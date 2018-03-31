2c3f91708e646e48298131a942778cfd6044d911
Evan Genest <genest.1@osu.edu>
2018-03-31 10:51:18 -0500

    Add an error-catcher.  I feel good that from scratch I mostly solved this problem, with help from eloquentjavascript.net  book.

--- arrrgs2.js
+++ arrrgs2.js
@@ -1,8 +1,23 @@
-let length1;
-if (undefined === length1){
-	length1 = [];
+"use strict";
+
+
+const checker=(a, b, c)=>{
+	let length1;
+	try {
+		length1 = Array.from(undefined);
+	} catch(error){
+		console.log(`Caught this _${error}_ but kept going.`);
+		length1 = 0;
+	}
 }
-console.log(`Hey the array length result is now _${length1.length}_`);
+
+
+console.log(`Hey the array length result is now _${checker()}_`);
+console.log(`Hey the array length result is now _${checker(3,2)}_`);
+
+
+
+
 
 // this seems to work
 
