b3cfed1f3da3107989c572d035f96721b631a0e6
Evan Genest <genest.1@osu.edu>
2018-04-02 12:23:19 -0500

    // write a Python style, modern ECMA6 for-loop using your F(LEOA) mnemonic

--- ent.js
+++ ent.js
@@ -3,8 +3,22 @@
 // Throw an error and catch it
 
 
+// next 
+// write a Python style, modern ECMA6 for-loop
+// implement from memory error throw - catch
 
 
+
+function fireworks(a){
+	let tigers = [6, 4, 33]; 
+	let mudhens = Array.from(tigers);
+	for(let x of mudhens){
+		console.log(`Kapow, ${x}`);
+	}
+}
+
+fireworks();
+fireworks(3, "yeah");
 
 // calling this "ent.js" because my mnemonic:
 
