9f1968aa5f8b82e4ee884d12d1c31ecad8fbe348
Evan Genest <genest.1@osu.edu>
2018-04-02 07:59:51 -0500

    Typing from memory.  'Eloquent Javascript' syntax for throw catch errors

---
+++ ent.js
@@ -0,0 +1,13 @@
+// Evan mistergenest@twitter April 2, 2018
+// SIMPLE TYPING FROM MEMORY: 
+// Throw an error and catch it
+
+
+
+
+
+// calling this "ent.js" because my mnemonic:
+
+// DRY      CATCH ERROR       E.N.T. bkwds
+
+// try{} catch(error){}       throw new Error("yuck" + error);
