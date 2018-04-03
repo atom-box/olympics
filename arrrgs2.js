d4af1c834c4200f5ffa48991414dad6ec4f0e118
Evan Genest <genest.1@osu.edu>
2018-04-03 18:40:01 -0500

    Expand this demo snippet to three functions.  Show how arrays get passed in.

--- arrrgs2.js
+++ arrrgs2.js
@@ -1,24 +1,58 @@
-// Rearrange try, catch, printf statements, don't change functionality too much.  version 2 of args catcher.
+//   Succesful rearrange -- this version doesn't work at all BUT cheerfully rolls along, complaining but NOT crashing
+
 
 "use strict";
 
 
-const checker=(a, b, c)=>{
+const nought=()=>{
 	console.log(`^^^^^`);
-	let someArgs;
+	let someArgs = [];
 	try {
 		someArgs = Array.from(arguments);
 	} catch(error){
-		console.log("Pickle.")
+		console.log("Pickle in 'nought'.")
 	}
 	console.log(`Your array has _${someArgs}_`);
 };
 
-checker("wren");
-checker(["wren", "horse"]); 
-checker();
-checker(2);
+const mono=(a)=>{
+	console.log(`!!!!!!!!!`);
+	let someArgs = [];
+	try {
+		someArgs = Array.from(a);
+	} catch(error){
+		console.log("Pickle in 'mono'.")
+	}
+	console.log(`Your array has _${someArgs}_`);
+};
 
+const tri=(a, b, c)=>{
+	console.log(`((((((()))))))`);
+	let someArgs = [];
+	try {
+		someArgs = Array.from(a, b, c);
+	} catch(error){
+		console.log("Pickle in 'tri'.")
+	}
+	console.log(`Your array has _${someArgs}_`);
+};
+
+
+
+nought("wren");				// CRASH
+nought(["wren", "horse"]); 	// CRASH
+nought();					// CRASH
+nought(11, 13, 17);			// CRASH
+
+mono("wren");     // works.  letter-array
+mono(["wren", "horse"]); // works
+mono();					// silent
+mono(11, 13, 17);		// silent
+
+tri("wren");    // works.  letter-array
+tri(["wren", "horse"]);  // works
+tri();					// silent
+tri(11, 13, 17);		// silent
 
 
 
