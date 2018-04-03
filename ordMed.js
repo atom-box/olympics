53c70fc9d1c69965f800143479dd5ccb31d1e422
Evan Genest <genest.1@osu.edu>
2018-04-03 18:58:24 -0500

    Snippet.  Shows that FUNC(a, b) can print 'b' without any C++ fancy syntax like ARGUMENTS[1]

---
+++ ordMed.js
@@ -0,0 +1,13 @@
+// snippet from OLYMPICsqlIte project 
+// Fifth Project in Codecademy 
+// April 3, 2018
+
+const orderedMedals = (country, field, sortAscending) => {
+	console.log(`Wow. Here is "field" from your 2nd array-element: _${field}_`);
+}
+
+orderedMedals([100, 200, 300]); // UNDEFINED!
+orderedMedals(100, 200, 300); // 200
+
+
+// That was easy.  Contradicts arrrgs.js though
